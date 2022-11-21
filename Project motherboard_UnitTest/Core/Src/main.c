b44cdc20a3d17797b7d8702c9c47c0e926e08431
nguyenngocsang1410 <nguyenngocsang1410@gmail.com>
2022-11-21 14:25:37 +0700

    Second commit

--- Project motherboard_UnitTest/Core/Src/main.c
+++ Project motherboard_UnitTest/Core/Src/main.c
@@ -22,7 +22,6 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "test_handler.h"
-//Test push
 
 /* USER CODE END Includes */
 
