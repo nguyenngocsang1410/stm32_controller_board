2b8e84dfbcec6d6852ede957e4d05c5cd320f6ff
nguyenngocsang1410 <nguyenngocsang1410@gmail.com>
2022-11-21 14:14:19 +0700

    First commit from unitTest

--- Project motherboard_UnitTest/Core/Src/main.c
+++ Project motherboard_UnitTest/Core/Src/main.c
@@ -22,6 +22,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "test_handler.h"
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
