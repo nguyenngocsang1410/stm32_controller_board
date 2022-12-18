9abcc2db57aa9f992ca6cda61a264bf4a5067370
nguyenngocsang1410 <nguyenngocsang1410@gmail.com>
2022-12-18 07:32:57 +0700

    find

--- Re_Project_Glove_Cut/Core/Src/user_code/UserCode.h
+++ Re_Project_Glove_Cut/Core/Src/user_code/UserCode.h
@@ -13,8 +13,8 @@
 
 #define FLASH_ADDR_PAGE_126 (u32)0x0801F810
 #define FLASH_ADDR_PAGE_127 (u32)0x0801FC00
-#define FLASH_USER_START_ADDR	FLASH_ADDR_PAGE_126
-#define FLASH_USER_END_ADDR     FLASH_ADDR_PAGE_127 + FLASH_PAGE_SIZE
+#define FLASH_USER_START_ADDR    FLASH_ADDR_PAGE_126
+#define FLASH_USER_END_ADDR     (FLASH_ADDR_PAGE_127 + FLASH_PAGE_SIZE)
 
 extern u8 mSpeed;
 extern u32 mSec;
@@ -33,38 +33,54 @@ extern TIM_HandleTypeDef htim4;
 
 extern u8 state;
 
-void start_up();
-void main_loop();
+void setup();
+
+void loop();
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin);
+
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim);
 
 void check_state();
+
 void check_ecd();
 
 void LED_ON();
+
 void LED_OFF();
+
 void LED_TOGGLE();
 
 void FLASH_WritePage(uint32_t startPage, uint32_t endPage, uint32_t data);
+
 uint32_t FLASH_ReadData(uint32_t addr);
 
 void printDefaultLCD();
+
 void update_Menu1();
+
 void update_Menu2();
+
 void update_Menu3();
 
 void LCD_Print_Clock(u32 sec);
+
 void printMenuName();
+
 void printMenuValue();
+
 void blinkCursor();
+
 void updateSelection();
 
 void set_motor(u8 id, s8 dir, u16 val);
 
 s32 map(s32 x, s32 in_min, s32 in_max, s32 out_min, s32 out_max);
+
 void reset_state();
+
 void save_menu_value();
 
 void stopCmd();
+
 #endif
